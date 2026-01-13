f6f941a22efad4e0f86cf6ec74d71c47ee1aa0d6
Anushri <sastikaranushri@gmail.com>
2026-01-13 11:57:54 -0500

    using window function

---
+++ rank_scores.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Scores (
+    id INT,
+    score DECIMAL(3,2)
+);
+
+
+INSERT INTO scores (id, score) VALUES
+(1, 3.50),
+(2, 3.65),
+(3, 4.00),
+(4, 3.85),
+(5, 4.00),
+(6, 3.65);
+
+SELECT * FROM Scores
+
+-- Window Function Approach
+SELECT score, DENSE_RANK() OVER(order by score DESC) as rnk FROM Scores
