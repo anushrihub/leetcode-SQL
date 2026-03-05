ef34b2eaf175475105c5f94ff0000edb09e65516
Anushri <sastikaranushri@gmail.com>
2026-03-05 16:18:27 -0500

    addressed follow up 1

--- friend_requests_I:_overall_acceptance_rate.sql
+++ friend_requests_I:_overall_acceptance_rate.sql
@@ -25,14 +25,31 @@ INSERT INTO RequestAccepted_I (requester_id, accepter_id, accept_date) VALUES
 (3, 4, '2016-06-09');
 
 -- acceptance rate
-SELECT
-ROUND(
-(IFNULL(
-    (SELECT COUNT(*) FROM (SELECT DISTINCT accept_date, requester_id, accepter_id FROM RequestAccepted) AS accepter)
-    /
-    (SELECT COUNT(*) FROM (SELECT DISTINCT request_date, sender_id, send_to_id FROM FriendRequest
-    ) AS sender),0)),2)
+With T1 as (
+    Select count(distinct sender_id, send_to_id) as sr
+    from FriendRequest
+),
+T2 as (
+    Select count(distinct requester_id, accepter_id) as ar from 
+    RequestAccepted
+)
+Select round(ifnull(ar/ sr, 0),2) AS accept_rate from T1 join T2
 
 -- acceptance rate by everymonth
+
+
+With T1 as (
+    Select MONTH(request_date) AS m,count(distinct sender_id, send_to_id) as sr
+    from FriendRequest
+    GROUP BY m
+),
+T2 as (
+    Select MONTH(accept_date) AS m ,count(distinct requester_id, accepter_id) as ar from 
+    RequestAccepted
+    GROUP BY m
+)
+Select round(ifnull(ar/ sr, 0),2) AS accept_rate from T1 inner join T2
+on T1.m = T2.m
+
 
 -- cummulative acceptance rate for every day
