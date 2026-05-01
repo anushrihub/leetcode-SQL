4a1a0ca5060bfafe1f29182a455cc4e8122038c9
Anushri <sastikaranushri@gmail.com>
2026-05-01 14:19:05 -0400

    using sqlite3

---
+++ list_the_products_ordered_in_a_period.py
@@ -0,0 +1,49 @@
+import sqlite3
+import pandas
+
+conn = sqlite3.connect(":memory")
+
+cursor = conn.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS Products ( product_id INTEGER,product_name varchar, product_category varchar)")
+
+cursor.execute("CREATE TABLE IF NOT EXISTS Orders ( order_id INTEGER,product_id  INTEGER, order_date  DATE, unit INTEGER)")
+
+cursor.executemany("INSERT INTO Products(product_id, product_name, product_category) VALUES (?, ?, ?)",[
+    (1, 'Leetcode Solutions',    'Book'),
+(2, 'Jewels of Stringology', 'Book'),
+(3, 'HP',                    'Laptop'),
+(4, 'Lenovo',                'Laptop'),
+(5, 'Leetcode Kit',          'T-shirt')
+])
+
+cursor.executemany("INSERT INTO Orders(product_id, order_date, unit) VALUES (?, ?, ?)",[
+(1, '2020-02-05', 60),
+(1, '2020-02-10', 70),
+(2, '2020-01-18', 30),
+(2, '2020-02-11', 80),
+(3, '2020-02-17',  2),
+(3, '2020-02-24',  3),
+(4, '2020-03-01', 20),
+(4, '2020-03-04', 30),
+(4, '2020-03-04', 60),
+(5, '2020-02-25', 50),
+(5, '2020-02-27', 50),
+(5, '2020-03-01', 50)
+])
+
+cursor.execute(""" SELECT product_name, SUM(unit) AS unit
+    FROM Products
+    INNER JOIN Orders ON Products.product_id = Orders.product_id
+    WHERE strftime('%Y', order_date) = '2020'
+      AND strftime('%m', order_date) = '02'
+    GROUP BY product_name
+    HAVING SUM(unit) >= 100""")
+
+
+rows = cursor.fetchall()
+
+print(f"{'product_name'}, {'unit'}")
+
+for row in rows:
+    print(f"{row[0]}, {row[1]}")
